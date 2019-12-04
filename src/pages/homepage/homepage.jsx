b7c8cb95533ecd7453bcab1a058c9e2fa8f6a0c0
Graham Cottridge <codinggc@gmail.com>
2019-12-04 16:59:21 +0000

    Create custom useHomeFetch component to connect with API

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from "../../config";
 
 import Grid from "../../components/grid/grid.component";
 import HeroImage from "../../components/hero-image/hero-image.component";
@@ -7,15 +9,22 @@ import MovieThumb from "../../components/movie-thumb/movie-thumb.component";
 import SearchBar from "../../components/search-bar/search-bar.component";
 import Spinner from "../../components/spinner/spinner.component";
 
-const HomePage = () => (
-  <>
-    <HeroImage />
-    <SearchBar />
-    <Grid />
-    <MovieThumb />
-    <Spinner />
-    <LoadMoreBtn />
-  </>
-);
+import { useHomeFetch } from "../../hooks/useHomeFetch";
+
+const HomePage = () => {
+  const [{ state, loading, error }, fetchMovies] = useHomeFetch();
+  console.log(state);
+
+  return (
+    <>
+      <HeroImage />
+      <SearchBar />
+      <Grid />
+      <MovieThumb />
+      <Spinner />
+      <LoadMoreBtn />
+    </>
+  );
+};
 
 export default HomePage;
