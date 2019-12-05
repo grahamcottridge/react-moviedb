cbb3a717af8acc197acebaed901bb5e52081364f
Graham Cottridge <codinggc@gmail.com>
2019-12-05 00:54:25 +0000

    Create useMovieFetch hook

--- src/pages/movie/movie.jsx
+++ src/pages/movie/movie.jsx
@@ -7,16 +7,22 @@ import Navigation from "../../components/navigation/navigation.component";
 import Grid from "../../components/grid/grid.component";
 import Spinner from "../../components/spinner/spinner.component";
 
-const Movie = ({ movieId }) => (
-  <>
-    <Navigation />
-    <MovieInfo />
-    <MovieInfoBar />
-    <Grid>
-      <Actor />
-    </Grid>
-    <Spinner />
-  </>
-);
+import { useMovieFetch } from "../../hooks/useMovieFetch";
+
+const Movie = ({ movieId }) => {
+  const [movie, loading, error] = useMovieFetch(movieId);
+  console.log(movie);
+  return (
+    <>
+      <Navigation />
+      <MovieInfo />
+      <MovieInfoBar />
+      <Grid>
+        <Actor />
+      </Grid>
+      <Spinner />
+    </>
+  );
+};
 
 export default Movie;
